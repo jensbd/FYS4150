d28d5e91dc76f0e2b0b3fe63a114caa0af14f6dc
jensbd <jensdue96@gmail.com>
2019-11-13 10:35:30 +0100

    Plots and plotter

--- Project4/plotter.py
+++ Project4/plotter.py
@@ -4,7 +4,7 @@ import numpy as np
 print("Which Project Task do you want to run")
 print("Task C - equilibrium plots, write c")
 print("Task D - probability histogram, write d")
-
+print("Task E & F - phase transitions and critical temperature, write e")
 Task = input("Write here: ")
 
 """
@@ -59,67 +59,61 @@ if Task == "c":
                     energyU2.append(float(pieces[1]))
                     magU2.append(float(pieces[2]))
                     NconfigsU2.append(float(pieces[3]))
-print(len(energyU))
-plt.figure()
-plt.title("Ordered")
-plt.plot(MCcycles, energyO)
-plt.plot(MCcycles,energyO2)
-#plt.axis([0, 5000,-2.1,-1.8])
-plt.legend(["T = 1.0","T = 2.4"])
-plt.xlabel("# of Monte Carlo cycles")
-plt.ylabel("Energy expectation value $\langle$E$\\rangle$")
+    plt.figure()
+    plt.title("Ordered")
+    plt.plot(MCcycles, energyO)
+    plt.plot(MCcycles,energyO2)
+    plt.legend(["T = 1.0","T = 2.4"])
+    plt.xlabel("# of Monte Carlo cycles")
+    plt.ylabel("Energy expectation value $\langle$E$\\rangle$")
 
-plt.figure()
-plt.title("Unordered")
-plt.plot(MCcycles, energyU)
-plt.plot(MCcycles,energyU2)
-#plt.axis([0, 5000,-2.1,-1.8])
-plt.legend(["T = 1.0","T = 2.4"])
-plt.xlabel("# of Monte Carlo cycles")
-plt.ylabel("Energy expectation value $\langle$E$\\rangle$")
+    plt.figure()
+    plt.title("Unordered")
+    plt.plot(MCcycles, energyU)
+    plt.plot(MCcycles,energyU2)
+    plt.legend(["T = 1.0","T = 2.4"])
+    plt.xlabel("# of Monte Carlo cycles")
+    plt.ylabel("Energy expectation value $\langle$E$\\rangle$")
 
-plt.figure()
-plt.title("Ordered")
-plt.plot(MCcycles, magO, "")
-plt.plot(MCcycles, magO2, "")
-#plt.axis([0, 5000,-2.1,-1.8])
-plt.legend(["T = 1.0","T = 2.4"])
-plt.xlabel("# of Monte Carlo cycles")
-plt.ylabel("Magnetization expectation value $\langle$M$\\rangle$")
+    plt.figure()
+    plt.title("Ordered")
+    plt.plot(MCcycles, magO, "")
+    plt.plot(MCcycles, magO2, "")
+    plt.legend(["T = 1.0","T = 2.4"])
+    plt.xlabel("# of Monte Carlo cycles")
+    plt.ylabel("Magnetization expectation value $\langle$|M|$\\rangle$")
 
-plt.figure()
-plt.title("Unordered")
-plt.plot(MCcycles, magU, "")
-plt.plot(MCcycles, magU2, "")
-#plt.axis([0, 5000,-2.1,-1.8])
-plt.legend(["T = 1.0","T = 2.4"])
-plt.xlabel("# of Monte Carlo cycles")
-plt.ylabel("Magnetization expectation value $\langle$M$\\rangle$")
+    plt.figure()
+    plt.title("Unordered")
+    plt.plot(MCcycles, magU, "")
+    plt.plot(MCcycles, magU2, "")
+    plt.legend(["T = 1.0","T = 2.4"])
+    plt.xlabel("# of Monte Carlo cycles")
+    plt.ylabel("Magnetization expectation value $\langle$|M|$\\rangle$")
 
-plt.figure()
-plt.title("Unordered")
-plt.plot(MCcycles, NconfigsU, "")
-plt.plot(MCcycles, NconfigsU2, "")
-#plt.axis([0, 5000,-2.1,-1.8])
-plt.legend(["T = 1.0","T = 2.4"])
-plt.xlabel("# of Monte Carlo cycles")
-plt.ylabel("Accepted configurations (normalized)")
+    plt.figure()
+    plt.title("Unordered")
+    plt.plot(MCcycles, NconfigsU, "")
+    plt.plot(MCcycles, NconfigsU2, "")
+    plt.legend(["T = 1.0","T = 2.4"])
+    plt.xlabel("# of Monte Carlo cycles")
+    plt.ylabel("Accepted configurations (normalized)")
 
-"""
-plt.figure()
-plt.plot(MCcycles, Cv, "")
-#plt.axis([0, 5000,-2.1,-1.8])
-plt.xlabel("# of Monte Carlo cycles")
-plt.ylabel("Specific heat $\langle$$C_v$$\\rangle$")
-"""
-"""
-plt.figure()
-plt.plot(MCcycles, susceptibility, "")
-#plt.axis([0, 5000,-2.1,-1.8])
-plt.xlabel("# of Monte Carlo cycles")
-plt.ylabel("Susceptibility $\langle$$\chi$$\\rangle$")
-"""
-plt.show()
+    Temp = []
+    configs = []
+    with open("Nconfig_vs_Temp") as file:
+        lines = file.readlines()
+        for i in range(2,len(lines)):
+
+            pieces = lines[i].split()
+            Temp.append(float(pieces[0]))
+            configs.append(float(pieces[1]))
+    plt.figure()
+    plt.plot(Temp,configs)
+    plt.xlabel("Temperature [kT/J]")
+    plt.ylabel("Accepted number of configurations (normalized)")
+    plt.title("Accepted number of configurations (normalized) as a function of T")
+    plt.show()
 
 """
 -------------
@@ -152,6 +146,96 @@ if Task == "d":
         plt.xlim(-805,-770) if i == "Probability_1" else plt.xlim(-705,-305)
         plt.xlabel("Energy")
         plt.ylabel("Energy counts")
+        if i == "Probability_1":
+            plt.title("T = 1.0")
+        else:
+            plt.title("T = 2.4")
         props = dict(boxstyle='round', facecolor='wheat', alpha=1)
         plt.text(0.05*(plt.xlim()[1]-plt.xlim()[0])+plt.xlim()[0] ,plt.ylim()[1]*0.85, "Most probable energy:\n" + str(most_probable_energy), bbox = props)
         plt.show()
+
+if Task == "e":
+    with open("Temperature") as file:
+        lines = file.readlines()
+    temps = []
+    energylist = []
+    maglist = []
+    Cvlist = []
+    Suscplist = []
+    indeks = 0
+    for i in range(1, len(lines)):
+        pieces = lines[i].split()
+        temps.append(float(pieces[0]))
+        energylist.append(float(pieces[1]))
+        maglist.append(float(pieces[2]))
+        Cvlist.append(float(pieces[3]))
+        Suscplist.append(float(pieces[4]))
+
+    firstTemp = temps[0]
+    for i in range(1,len(temps)):
+        if temps[i] == firstTemp:
+            temps = temps[0:i]
+            break
+
+    TCCv = []
+    TCX = []
+    for i in range(int(len(energylist)/len(temps))):
+        max_temp = 0
+        sublistCv = Cvlist[i*len(temps):len(temps)*(i+1)]
+        sublistSuscp = Suscplist[i*len(temps):len(temps)*(i+1)]
+        maxCv = max(sublistCv)
+        maxSuscp = max(sublistSuscp)
+        TCCv.append(temps[sublistCv.index(maxCv)])
+        TCX.append(temps[sublistSuscp.index(maxSuscp)])
+        print("TC for Cv =",temps[sublistCv.index(maxCv)])
+        print("TC for X =",temps[sublistSuscp.index(maxSuscp)])
+
+    plt.figure()
+    plt.title("Mean Energy")
+    plt.xlabel("T [kT/J]")
+    plt.ylabel("Energy expectation value $\langle$E$\\rangle$")
+    for i in range(int(len(energylist)/len(temps))):
+        plt.plot(temps,energylist[i*len(temps):len(temps)*(i+1)])
+    plt.legend(["L = 40","L = 60","L = 80","L = 100"])
+
+    plt.figure()
+    plt.title("Absolute mean Magnetization")
+    plt.xlabel("T [kT/J]")
+    plt.ylabel("Magnetization expectation value $\langle$|M|$\\rangle$")
+    for i in range(int(len(energylist)/len(temps))):
+        plt.plot(temps,maglist[i*len(temps):len(temps)*(i+1)])
+    plt.legend(["L = 40","L = 60","L = 80","L = 100"])
+
+    plt.figure()
+    plt.title("Specific heat")
+    plt.xlabel("T [kT/J]")
+    plt.ylabel("Specific heat $\langle$$C_v$$\\rangle$")
+    for i in range(int(len(energylist)/len(temps))):
+        plt.plot(temps,Cvlist[i*len(temps):len(temps)*(i+1)])
+    plt.legend(["L = 40","L = 60","L = 80","L = 100"])
+
+
+    plt.figure()
+    plt.title("Susceptibility")
+    plt.xlabel("T [kT/J]")
+    plt.ylabel("Susceptibility $\langle$$\chi$$\\rangle$")
+    for i in range(int(len(energylist)/len(temps))):
+        plt.plot(temps,Suscplist[i*len(temps):len(temps)*(i+1)])
+    plt.legend(["L = 40","L = 60","L = 80","L = 100"])
+    plt.show()
+
+    """
+    Task f)
+    """
+    #Performing a linear regression to find critical temp in thermodyn. limit
+    meanTC = 0.5*(np.array(TCCv) +np.array(TCX))
+    Llist = np.array([40,60,80,100])
+    Llist = 1.0/Llist
+
+    linreg = np.polyfit(Llist,meanTC,1)
+    values = np.polyval(linreg, np.linspace(2.275,2.325,100))
+    plt.plot(Llist,meanTC,"o")
+    plt.plot(Llist,np.polyval(linreg,Llist))
+    plt.legend(["Mean $T_C$","Linear fit = %gx + %g" % (linreg[0],linreg[1])])
+
+    plt.show()
