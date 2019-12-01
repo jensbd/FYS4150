7b789f10b7e6349e641ab0f62276c55e0f17579d
Msismail <mohe95@hotmail.no>
2019-12-01 12:53:07 +0100

    Changed Project 5

--- Project5/Functions.cpp
+++ Project5/Functions.cpp
@@ -2,50 +2,95 @@
 #include "omp.h"
 
 
-void forward_euler(double dx, mat &u, vec xlist, vec tlist){
-  double C = dt/(dx*dx)
-  double dt = 0.5*dx*dx //Stability criterion
-  vec u1 = u(1) //Initial condition, zeros
-  for(int j = 1; j < tlist.n_elem;  j++){
-    for (int i = 1; i < xlist.n_elem-1; i++){
-      u(j,i) = C*(u1(i+1) - 2*u1(i) + u1(i-1)) + u1(i)
-    }
-    // Boundaries need not be updated, they are already zeros by def. of u-matrix
-    u1 = u(j)
-  }
-}
+// Code for solving the 1+1 dimensional diffusion equation
+// du/dt = ddu/ddx on a rectangular grid of size L x (T*dt),
+// with with L = 1, u(x,0) = g(x), u(0,t) = u(L,t) = 0
 
-void backward_euler(double dx, mat &u, vec xlist, vec tlist){
 
-}
-void backward_euler(int n, int tsteps, double delta_x, double alpha)
-{
-   double a, b, c;
-   vec u(n+1); // This is u  of Au = y
-   vec y(n+1); // Right side of matrix equation Au=y, the solution at a previous step
 
-   // Initial conditions
-   for (int i = 1; i < n; i++) {
-      y(i) = u(i) = func(delta_x*i);
-   }
-   // Boundary conditions (zero here)
-   y(n) = u(n) = u(0) = y(0);
-   // Matrix A, only constants
-   a = c = - alpha;
-   b = 1 + 2*alpha;
-   // Time iteration
-   for (int t = 1; t <= tsteps; t++) {
-      //  here we solve the tridiagonal linear set of equations,
-      tridag(a, b, c, y, u, n+1);
-      // boundary conditions
-      u(0) = 0;
-      u(n) = 0;
-      // replace previous time solution with new
-      for (int i = 0; i <= n; i++) {
-	 y(i) = u(i);
+void forward_step(double alpha, vec &u, vec &uPrev, int N){
+    /*
+    Steps forward-euler algo one step ahead.
+    Implemented in a separate function for code-reuse from crank_nicolson()
+    */
+
+    for (int i = 1; i < N+1; i++){ //loop from i=1 to i=N
+        u(i) = alpha*uPrev(i-1) + (1.0-2*alpha)*uPrev(i) + alpha*uPrev(i+1);
       }
-      //  You may consider printing the solution at regular time intervals
-      ....   // print statements
-   }  // end time iteration
-   ...
+}
+
+void forward_euler(double alpha, mat &u, int N, int T){
+    /*
+    Implements the forward Euler sheme, results saved to
+    array u
+    */
+
+    // Skip boundary elements
+    for (int t = 1; t < T; t++){
+        forward_step(alpha,u(t),u(t-1),N);
+      }
+}
+
+void tridiag(double alpha, vec &u, int N){
+    /*
+    Tridiagonal gaus-eliminator, specialized to diagonal = 1+2*alpha,
+    super- and sub- diagonal = - alpha
+    */
+
+    vec d = zeros<mat>(N).fill(1+2*alpha);
+    vec b = zeros<mat>(N-1).fill(- alpha);
+
+    //Forward eliminate
+    for (int i = 1; i < N; i++){
+        //Normalize row i (i in u convention):
+        b(i-1) /= d(i-1);
+        u(i) /= d(i-1); //Note: row i in u = row i-1 in the matrix
+        d(i-1) = 1.0;
+        //Eliminate
+        u(i+1) += u(i)*alpha;
+        d(i) += b(i-1)*alpha;
+    }
+    //Normalize bottom row
+    u(N) /= d(N-1);
+    d(N-1) = 1.0;
+
+    // Backward substitute
+    for (int i = N; i > 1; i--){ // loop from i=N to i=2
+        u(i-1) -= u(i)*b(i-2);
+        // b(i-2) = 0.0 // This is never read, why bother...
+      }
+
+}
+
+
+void backward_euler(double alpha, mat &u, int N, int T){
+    /*
+    Implements backward euler scheme by gaus-elimination of tridiagonal matrix.
+    Results are saved to u.
+    */
+
+    for (int t = 1; t < T; t++){
+        u(t) = u(t-1); // .copy() ?
+        tridiag(alpha,u(t),N); //Note: Passing a pointer to row t, which is modified in-place
+      }
+}
+
+void crank_nicolson(double alpha, mat &u, int N, int T){
+    /*
+    Implents crank-nicolson scheme, reusing code from forward- and backward euler
+    */
+    for (int t = 1; t < T; t++){
+        forward_step(alpha/2,u(t),u(t-1),N);
+        tridiag(alpha/2,u(t),N);
+      }
+}
+void g(vec &u, int N){
+    // Initial condition u(x,0) = g(x), x \in (0,1)
+
+    double pi = 4*atan(1); // This is the constant pi
+    double dx = 1.0/N;
+
+    for (int i = 1; i < N; i++){
+      u(i) = sin(pi*i*dx)
+  }
 }
--- Project5/Functions.h
+++ Project5/Functions.h
@@ -12,3 +12,12 @@
 
 using namespace  std;
 using namespace arma;
+
+
+
+void forward_step(double, vec &, vec &, int);
+void forward_euler(double, vec &, int, int);
+void tridiag(double, vec &, int);
+void backward_euler(double, vec &, int, int);
+void crank_nicolson(double, vec &, int, int);
+void g(vec &, int);
